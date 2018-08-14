88781042c4758670704433757aa3ac9743a2d050
Seongkwon <tjdrnjs2024@kaist.ac.kr>
2018-08-14 18:22:27 +0900

    init_state

---
+++ Swift/SelfStudy1/SelfStudy1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfStudy1
+//
+//  Created by sungkwon kim on 2018. 8. 14..
+//  Copyright © 2018년 cirale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
